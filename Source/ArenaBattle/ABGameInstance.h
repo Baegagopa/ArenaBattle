a71bac69bd4f0ce1e6015a79574e91fc11b107b8
MBDS <kbs99q@naver.com>
2017-03-07 13:37:05 +0900

    first

---
+++ Source/ArenaBattle/ABGameInstance.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine/GameInstance.h"
+#include "ABGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLE_API UABGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+public :
+	virtual void Init() override;
+	
+};
